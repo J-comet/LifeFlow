2f35bd5574a7cd5296153e41e1865718a659fe0b
hyeseong <hyeseong7848@gmail.com>
2023-11-13 14:18:18 +0900

    gitignore 추가

---
+++ LifeFlow/Utils/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  LifeFlow
+//
+//  Created by 장혜성 on 2023/11/13.
+//
+
+import Foundation
+
+struct Constant {
+    static let productName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as! String
+}
